1ba687d9f759f92fd3f54fd6344f494a2b77281b
FarnazRezvani <151848953+FarnazRezvani@users.noreply.github.com>
2024-12-25 21:58:43 +0000

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -2,7 +2,6 @@ import pytest
 from mean_var_std import calculate
 
 def test_calculate():
-    # تست ورودی‌های معتبر
     result = calculate([0,1,2,3,4,5,6,7,8])
     assert result['mean'] == [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0]
     assert result['variance'] == [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667]
@@ -11,9 +10,8 @@ def test_calculate():
     assert result['min'] == [[0, 1, 2], [0, 3, 6], 0]
     assert result['sum'] == [[9, 12, 15], [3, 12, 21], 36]
     
-    # تست ورودی‌های نامعتبر
     with pytest.raises(ValueError):
-        calculate([1, 2, 3])  # کمتر از 9 عدد
+        calculate([1, 2, 3])  
     
     with pytest.raises(ValueError):
-        calculate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])  # بیشتر از 9 عدد
+        calculate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])  
